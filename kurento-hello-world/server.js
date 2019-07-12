ed5a1a837bee1b5168dcea5d9897361521f0f1ac
Pooja Pathak <33315652+pooja1pathak@users.noreply.github.com>
2019-07-12 14:36:33 +0530

    Update server.js

--- kurento-hello-world/server.js
+++ kurento-hello-world/server.js
@@ -102,7 +102,8 @@ con.connect(function(err) {
             //var tmp = JSON.stringify(result);
             count = tmp.slice(27);
             //count = count.trim();
-            console.log("count: " + obj["COUNT(*)"]);
+            //console.log("count: " + obj["COUNT(*)"]);
+            console.log("obj: "+ obj);
         });
         con.query("SELECT cam_id, cam_addr FROM test_db", function (err, result, fields) {
             if (err) throw err;
